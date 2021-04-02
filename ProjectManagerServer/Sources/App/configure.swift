70486979b2a329fc0a0fe51b2227aad3ed610951
GREEN <humains29@gmail.com>
2021-04-02 15:02:06 +0900

    [#4] feat: Fluent 관련 라이브러리 configure.swift import

--- ProjectManagerServer/Sources/App/configure.swift
+++ ProjectManagerServer/Sources/App/configure.swift
@@ -1,14 +1,9 @@
 import Vapor
 import Fluent
-import FluentPostgresDriver
+import FluentMongoDriver
 
-// configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.postgres(hostname: "localhost", username: "vapor", password: "vapor", database: "vapor"), as: .psql)
-    try app.databases.use(.postgres(url: "<connection string>"), as: .psql)
-    // register routes
+    try app.databases.use(.mongo(connectionString: "<connection string>"), as: .mongo)
     try routes(app)
 }

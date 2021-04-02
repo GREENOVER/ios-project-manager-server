e6e6aa46f4a589466e53608ac91dd189ab540f05
GREEN <humains29@gmail.com>
2021-04-02 15:00:13 +0900

    [#4] refactor: FluentMongoDriver 라이브러리로 변경

--- ProjectManagerServer/Sources/App/configure.swift
+++ ProjectManagerServer/Sources/App/configure.swift
@@ -1,10 +1,14 @@
 import Vapor
+import Fluent
+import FluentPostgresDriver
 
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    app.databases.use(.postgres(hostname: "localhost", username: "vapor", password: "vapor", database: "vapor"), as: .psql)
+    try app.databases.use(.postgres(url: "<connection string>"), as: .psql)
     // register routes
     try routes(app)
 }

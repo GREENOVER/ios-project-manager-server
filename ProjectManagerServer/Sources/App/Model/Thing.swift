5f37d5dc11467853a4e16c46163fadf9b05841e4
GREEN <humains29@gmail.com>
2021-04-02 15:16:55 +0900

    [#4] feat: Thing 모델 구현

--- ProjectManagerServer/Sources/App/Model/Thing.swift
+++ ProjectManagerServer/Sources/App/Model/Thing.swift
@@ -2,5 +2,33 @@ import Vapor
 import Fluent
 
 final class Thing: Model, Content {
+    static let schema = "things"
     
+    @ID(key: .id)
+    var id: UUID
+    
+    @Field(key: "title")
+    var title: String?
+    
+    @Field(key: "description")
+    var description: String?
+    
+    @Field(key: "state")
+    var state: String
+    
+    @Field(key: "due_date")
+    var dueDate: Double
+    
+    @Timestamp(key: "updated_at", on: .update)
+    var updatedAt: Double
+    
+    init() {}
+    
+    init(id: UUID, title: String? = nil, description: String? = nil, state: String, dueDate: Double, updateAt: Double) {
+        self.id = id
+        self.name = name
+        self.fileName = fileName
+        self.fileType = fileType
+        self.content = content
+    }
 }

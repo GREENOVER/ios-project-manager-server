e48180b4dcac0d0f5ade5ea2989449e4995e10d9
GREEN <humains29@gmail.com>
2021-04-16 08:10:39 +0900

    feat: GroundRule 및 MockData 폴더 추가

--- Sources/App/Controllers/ThingError.swift
+++ Sources/App/Controllers/ThingError.swift
@@ -1,10 +1,3 @@
-//
-//  ThingError.swift
-//  
-//
-//  Created by 임성민 on 2021/04/13.
-//
-
 import Vapor
 
 enum ThingError {
--- Sources/App/Model/ThingToUpdate.swift
+++ Sources/App/Model/ThingToUpdate.swift
@@ -1,10 +1,3 @@
-//
-//  File.swift
-//  
-//
-//  Created by 임성민 on 2021/04/13.
-//
-
 import Vapor
 
 struct ThingToUpdate: Content {
